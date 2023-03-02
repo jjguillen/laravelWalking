616cea4fb90fbb1c7f9ab40080a5dd45bf909bbb
jjguillen <jjavierguillen@gmail.com>
2023-03-02 15:06:19 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\SenderoCollection;
+use App\Http\Resources\GrupoResource;
+use App\Models\Sendero;
+use App\Models\Grupo;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +24,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('senderos/',  function () {
+        return new SenderoCollection(Sendero::all());
+    });
+
+});
+
+
+Route::get('grupos/{id}',  function ($id) {
+    return new GrupoResource(Grupo::findOrFail($id));
+});
+
+Route::get('grupos/',  function () {
+    return GrupoResource::collection(Grupo::all());
+});
+
+//CREAR TOKEN
+Route::post('/tokens/create', function (Request $request) {
+  
+    $user = User::where('email', $request->email)->first();
+  
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+          'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $token = $user->createToken($request->email);
+ 
+    return response()->json(['token' => $token->plainTextToken]);
 });
